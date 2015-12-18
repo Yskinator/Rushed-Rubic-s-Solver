0e7e7cf06fb3bc8e56a27cb2f7202468cffd2879
Yskinator <yski2moreletters@gmail.com>
2015-12-19 00:06:32 +0200

    Partially completed the clockwise rotation of the front of the cube.

--- PythonPrototype/rubic.py
+++ PythonPrototype/rubic.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3.5
 
 class Rubic:
-    F = [[0 for x in range(3)] for y in range(3)]
+    F = [[""+str(x)+str(y) for x in range(3)] for y in range(3)]
     L = [[1 for x in range(3)] for y in range(3)]
     R = [[2 for x in range(3)] for y in range(3)]
     U = [[3 for x in range(3)] for y in range(3)]
     D = [[4 for x in range(3)] for y in range(3)]
     B = [[5 for x in range(3)] for y in range(3)]
 
-    #Capital letter equals clockwise rotation
-    
+    #Capital letter means clockwise rotation
+    #Two letters mean the first two layers
+    def dF(self):
+        nF = [["" for x in range(3)] for y in range(3)]
+        for y in range(3):
+            for x in range(3):
+                nF[x][y] = self.F[2-y][x]
+        self.F = nF
+
+        
 
