c82275233d35dcb1c1b28fbbb1f7207273a13e1c
Yskinator <yski2moreletters@gmail.com>
2015-12-18 20:31:03 +0200

    Started working on the Python version. I am writing the solver in Python first, because it is much faster to work with - it'll likely ultimately save time even though I have to port it over to fortran for the final version.

---
+++ PythonPrototype/rubic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.5
+
+class Rubic:
+    F = [[0 for x in range(3)] for y in range(3)]
+    L = [[1 for x in range(3)] for y in range(3)]
+    R = [[2 for x in range(3)] for y in range(3)]
+    U = [[3 for x in range(3)] for y in range(3)]
+    D = [[4 for x in range(3)] for y in range(3)]
+    B = [[5 for x in range(3)] for y in range(3)]
+
+    #Capital letter equals clockwise rotation
+    
+
